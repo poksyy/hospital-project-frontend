4b1cdcfbc1bd910f299ea826d3cf54cb82604328
poksy <poksy2002@gmail.com>
2024-12-10 16:38:57 +0100

    FEAT(nurseList, homeScreen): add nurses with ViewModel and enhance UI
    - Improve nurse list UI with photos and better information structure
    
    - Update back button functionality
    
    Closes #11.

--- app/src/main/java/com/example/hospital/ui/nurses/list/NursesListViewModel.kt
+++ app/src/main/java/com/example/hospital/ui/nurses/list/NursesListViewModel.kt
@@ -1,2 +1,19 @@
-package com.example.hospital.ui.nurses.list
+import androidx.lifecycle.ViewModel
+import com.example.hospital.R
+import com.example.hospital.ui.nurses.list.Nurse
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
+class NursesListViewModel : ViewModel() {
+    private val _nurses = MutableStateFlow<List<Nurse>>(
+        listOf(
+            Nurse("Pau", "Carrera", R.drawable.nurse1, "Senior Nurse"),
+            Nurse("Cristian", "Oraña", R.drawable.nurse2, "Junior Nurse"),
+            Nurse("Dylan", "Navarro", R.drawable.nurse3, "Head Nurse"),
+            Nurse("Alex", "Rodríguez", R.drawable.nurse4, "Junior Nurse"),
+            Nurse("Dafne Michelle", "Ramírez", R.drawable.nurse5, "Junior Nurse"),
+            Nurse("Noemí", "Saladie", R.drawable.nurse6, "Senior Nurse")
+        )
+    )
+    val nurses: StateFlow<List<Nurse>> get() = _nurses
+}
