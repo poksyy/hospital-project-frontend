64bae460935902d0a8c8d84329f7cfaf7a45b5bd
krschan <krischxn@gmail.com>
2025-01-29 11:41:25 +0100

    REFACTOR(RegisterScreen): Improve layout

--- app/src/main/java/com/example/hospital/ui/auth/RegistrerScreen.kt
+++ app/src/main/java/com/example/hospital/ui/auth/RegistrerScreen.kt
@@ -40,128 +40,136 @@ fun RegisterScreen(navController: NavController, viewModel: AuthViewModel = view
             is RegisterResult.Success -> {
                 scope.launch {
                     val result =
-                            snackbarHostState.showSnackbar(
-                                    message = "Successfully registered!",
-                                    duration = SnackbarDuration.Short
-                            )
+                        snackbarHostState.showSnackbar(
+                            message = "Successfully registered!",
+                            duration = SnackbarDuration.Short
+                        )
                     if (result == SnackbarResult.Dismissed ||
-                                    result == SnackbarResult.ActionPerformed
+                        result == SnackbarResult.ActionPerformed
                     ) {
                         navController.navigate("login") { popUpTo("register") { inclusive = true } }
                     }
                 }
             }
+
             is RegisterResult.Failure -> {
                 errorMessage = registerResult.error
             }
+
             else -> Unit // Do nothing for other states
         }
     }
 
     Column(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(125.dp))
 
         Image(
-                painter = painterResource(id = R.drawable.hospital_logo),
-                contentDescription = "Hospital Logo",
-                modifier = Modifier.size(250.dp)
+            painter = painterResource(id = R.drawable.hospital_logo),
+            contentDescription = "Hospital Logo",
+            modifier = Modifier.size(250.dp)
         )
 
         Spacer(modifier = Modifier.height(32.dp))
 
         Text(
-                text = "Hospital Management",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold
+            text = "Hospital Management",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
         Text(
-                text = "Create a new account",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(bottom = 32.dp)
+            text = "Create a new account",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(bottom = 32.dp)
         )
 
         OutlinedTextField(
-                value = user,
-                onValueChange = { user = it },
-                label = { Text("User") },
-                modifier = Modifier.width(350.dp)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                singleLine = true,
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.width(350.dp)
+            value = user,
+            onValueChange = { user = it },
+            label = { Text("User") },
+            modifier = Modifier.width(350.dp)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
         OutlinedTextField(
-                value = confirmPassword,
-                onValueChange = { confirmPassword = it },
-                label = { Text("Confirm Password") },
-                singleLine = true,
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.width(350.dp)
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.width(350.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.width(350.dp)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
         errorMessage?.let {
             Text(
-                    text = it,
-                    color = MaterialTheme.colorScheme.error,
-                    modifier = Modifier.padding(vertical = 8.dp)
+                text = it,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(vertical = 8.dp)
             )
         }
 
         Button(
-                onClick = {
-                    scope.launch {
-                        if (password != confirmPassword) {
-                            errorMessage = "Passwords don't match"
-                            return@launch
-                        }
-
-                        viewModel.username = user
-                        viewModel.password = password
-
-                        viewModel.register(user, password)
+            onClick = {
+                scope.launch {
+                    if (password != confirmPassword) {
+                        errorMessage = "Passwords don't match"
+                        return@launch
                     }
-                },
-                modifier = Modifier.width(350.dp).padding(vertical = 5.dp),
-                colors =
-                        ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.primary,
-                                contentColor = Color.White
-                        )
+
+                    viewModel.username = user
+                    viewModel.password = password
+
+                    viewModel.register(user, password)
+                }
+            },
+            modifier = Modifier
+                .width(350.dp)
+                .padding(vertical = 5.dp),
+            colors =
+            ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            )
         ) { Text("Register") }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         TextButton(onClick = { navController.popBackStack() }) {
             Text(
-                    text = "Back to Login",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.secondary
+                text = "Back to Login",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.secondary
             )
         }
     }
 
     // Display the snackbar at the bottom of the screen
     Box(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
-            contentAlignment = Alignment.BottomCenter
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.BottomCenter
     ) { SnackbarHost(hostState = snackbarHostState) }
 }
